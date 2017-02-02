e094e21d21fc0177fdb8198fd2ada67b539ca685
Nikola Ruzic <nruzic.nixanet@gmail.com>
2017-02-02 15:23:29 +0100

    fixing default service locator commit

--- nxnet-commons-mvnutils/src/main/java/io/nxnet/commons/mvnutils/pom/resolver/impl/DefaultServiceLocator.java
+++ nxnet-commons-mvnutils/src/main/java/io/nxnet/commons/mvnutils/pom/resolver/impl/DefaultServiceLocator.java
@@ -14,4 +14,33 @@ import io.nxnet.commons.mvnutils.pom.resolver.RepositorySystemSessionFactory;
 import io.nxnet.commons.mvnutils.pom.resolver.ServiceLocator;
 
 public class DefaultServiceLocator extends ServiceLocator
-{}
+{
+    private static final Map<Class<?>, Object> services = initServices();
+
+    @Override
+    public <S> S getService(Class<S> clazz)
+    {
+        S service = (S)services.get(clazz);
+        if (service == null)
+        {
+            throw new IllegalStateException("Unknown service of type " + clazz);
+        }
+        
+        return service;
+    }
+
+    private static Map<Class<?>, Object> initServices()
+    {
+        Map<Class<?>, Object> services = new HashMap<Class<?>, Object>();
+        services.put(DependencyResolver.class, new DefaultDependencyResolver());
+        services.put(LocalRepositoryFactory.class, new DefaultLocalRepositoryFactory());
+        services.put(ModelFactory.class, new DefaultModelFactory());
+        services.put(ProxyDefinitionFactory.class, new LocalhostProxyDefinitionFactory());
+        services.put(RemoteRepositoryFactory.class, new EtkcRemoteRepositoryFactory());
+        services.put(RemoteRepositoryManagerFactory.class, new DefaultRemoteRepositoryManagerFactory());
+        services.put(RepositorySystemFactory.class, new DefaultRepositorySystemFactory());
+        services.put(RepositorySystemSessionFactory.class, new DefaultRepositorySystemSessionFactory());
+        
+        return services;
+    }
+}
